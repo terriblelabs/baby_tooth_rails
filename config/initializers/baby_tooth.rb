b7c173b426e0b0ac5b0ae4fe5f2c99d39560129b
Joe Lind <joelind@gmail.com>
2011-09-11 13:01:05 -0400

    Add baby_tooth initializer

---
+++ config/initializers/baby_tooth.rb
@@ -0,0 +1,9 @@
+
+BabyTooth.configure do |config|
+  config.client_id         = "ADDME"
+  config.client_secret     = "ADDME"
+  config.access_token_url  = "https://runkeeper.com/apps/token"
+  config.authorization_url = "https://runkeeper.com/apps/authorize"
+  config.site              = "http://api.runkeeper.com"
+  config.redirect_uri      = "ADDME"
+end
